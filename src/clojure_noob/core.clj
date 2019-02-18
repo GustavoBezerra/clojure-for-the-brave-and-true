157d228ad5080a9fee2702fcb70ca400270a31ff
Gustavo de Souza Bezerra <gustavo.bezerra@hotmail.com>
2019-02-18 17:00:05 -0300

    First commit

---
+++ src/clojure_noob/core.clj
@@ -0,0 +1,13 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+(println "You held your head like a hero")
+(println "On a history book page")
+(println "It was the end of a decade")
+(println "But the start of an age")
+(println "- Taylor Swift")
+(println (/ 6 3))
+)
