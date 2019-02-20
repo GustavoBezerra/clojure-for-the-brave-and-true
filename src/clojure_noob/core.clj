16464f95d1adb0a98dfd3811e57c129d46de1949
Gustavo de Souza Bezerra <gustavo.bezerra@hotmail.com>
2019-02-19 22:16:33 -0300

    Conditions (if and when)

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -4,10 +4,23 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-(println "You held your head like a hero")
-(println "On a history book page")
-(println "It was the end of a decade")
-(println "But the start of an age")
-(println "- Taylor Swift")
-(println (/ 6 3))
+  (if true
+    (println "True!")     ;then
+    (println "False!"))   ;else
+  (if false
+    (println "True!")
+    (println "False!"))
+  (if true
+    (do 
+      (print "True")
+      (println " and Do")))
+
+  (when true                    ;when = if + do (don't have a else branch)
+    (println "Success!")
+    (println "abra cadabra"))
+
+  (println (nil? 1))  ;verify if is nil
+  (println (nil? nil))  ;verify if is nil
 )
+
+
